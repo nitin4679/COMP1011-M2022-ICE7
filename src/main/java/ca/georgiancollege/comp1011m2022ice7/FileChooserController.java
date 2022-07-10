9c4cc32167d40746b0fd29b4fde1e444f165d9a2
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-07-10 13:52:52 -0400

    Successfully performed simple file I/O

--- src/main/java/ca/georgiancollege/comp1011m2022ice7/FileChooserController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice7/FileChooserController.java
@@ -1,4 +1,46 @@
 package ca.georgiancollege.comp1011m2022ice7;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.nio.file.Paths;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class FileChooserController {
+
+    @FXML
+    private Button SaveToFileButton;
+
+    @FXML
+    void SaveToFileButtonClicked(ActionEvent event)
+    {
+        try(Formatter output = new Formatter("vector2d.txt") )
+        {
+            Vector2D firstVector = new Vector2D(0.0f, 10.0f);
+            // serialize our Vector2D data
+            output.format("%.1f %.1f", firstVector.getX(), firstVector.getY());
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    @FXML
+    void ReadFromFileButtonClicked(ActionEvent event)
+    {
+        try(Scanner input = new Scanner(Paths.get("vector2d.txt")))
+        {
+            while(input.hasNext())
+            {
+                System.out.printf("(%.1f, %.1f)", input.nextFloat(), input.nextFloat());
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
 }
