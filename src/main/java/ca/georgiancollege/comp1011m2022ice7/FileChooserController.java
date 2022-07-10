64ddf5a8cde88af49be8fb617cc19402b8e3c8de
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-07-10 14:59:55 -0400

    implemented simple FileChooser

--- src/main/java/ca/georgiancollege/comp1011m2022ice7/FileChooserController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice7/FileChooserController.java
@@ -3,21 +3,37 @@ package ca.georgiancollege.comp1011m2022ice7;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.Scanner;
 
-public class FileChooserController {
+public class FileChooserController
+{
+    private String activeFilePath = "vector2d.txt";
+    @FXML
+    private BorderPane borderPane;
 
+    @FXML
+    private Button ReadFromFileButton;
     @FXML
     private Button SaveToFileButton;
 
     @FXML
+    private Button SelectFileButton;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
     void SaveToFileButtonClicked(ActionEvent event)
     {
-        try(Formatter output = new Formatter("vector2d.txt") )
+        try(Formatter output = new Formatter(activeFilePath) )
         {
             ArrayList<Vector2D> vector2DData = DBManager.Instance().readVectorTable();
 
@@ -39,7 +55,7 @@ public class FileChooserController {
     @FXML
     void ReadFromFileButtonClicked(ActionEvent event)
     {
-        try(Scanner input = new Scanner(Paths.get("vector2d-old.txt")))
+        try(Scanner input = new Scanner(Paths.get(activeFilePath)))
         {
             while(input.hasNext())
             {
@@ -49,6 +65,35 @@ public class FileChooserController {
         catch(Exception exception)
         {
             exception.printStackTrace();
+        }
+    }
+    @FXML
+    void SelectFileButtonClicked(ActionEvent event)
+    {
+        // configure dialog (modal window) allowing selection of a file
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files (*.*)", "*.*")
+        );
+
+
+        // display files in folder from which the app was launched
+        fileChooser.setInitialDirectory(new File("."));
+
+        // display the FileChooser Dialog
+        File file = fileChooser.showOpenDialog(borderPane.getScene().getWindow());
+
+
+        // process selected Path or display a message
+        if(file != null)
+        {
+            activeFilePath = file.getName();
+        }
+        else
+        {
+            textArea.setText("Select file");
         }
     }
 }
